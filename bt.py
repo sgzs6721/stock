1ef5567a58603dff57bb9ba8937e6ab5c5e1e89d
sgzs <sgzs6721@126.com>
2016-11-04 00:37:28 +0800

    add big trade record

---
+++ bt.py
@@ -0,0 +1,60 @@
+#encoding: utf-8
+import urllib2
+import time
+from pprint import pprint
+# from bs4 import BeautifulSoup
+from BeautifulSoup import BeautifulSoup
+import tushare as ts
+import MySQLdb
+
+def getSoup (url) :
+    times = 0
+    while times < 10 :
+        try :
+            req = urllib2.Request(url)
+            res = urllib2.urlopen(req, timeout = 15).read()
+            # return BeautifulSoup(res, "html.parser")
+            return BeautifulSoup(res)
+
+        except :
+            print "Could not get soup from " + url
+            print "Sleep 3 seconds and retrying ..."
+            time.sleep(3)
+            times = times + 1
+
+    return ""
+
+def getPageInfo(url, num, page) :
+    realURL = url + "?num=" + str(num) + "&p=" + str(page)
+    soup = getSoup(realURL)
+    trArray = soup.find(id='dataTable').findAll("tr")[1:]
+    record = {}
+    for index, tr in enumerate(trArray) :
+        td = tr.findAll("td")
+        record['date']        = td[0].text.encode("utf8")
+        record['num']         = td[1].a.text.encode("utf8")
+        record['name']        = td[2].a.text.encode("utf8")
+        record['dealprice']   = td[3].text.encode("utf8")
+        record['volumn']      = td[4].text.encode("utf8")
+        record['volumnmoney'] = td[5].text.encode("utf8")
+        record['buy']         = td[6].text.encode("utf8")
+        record['sell']        = td[7].text.encode("utf8")
+        record['type']        = td[8].text.encode("utf8")
+        pprint(record)
+        each = getStockInfo(record)
+        pprint(each)
+
+def getStockInfo(record) :
+    pprint(ts.get_realtime_quotes(record['num']))
+    return 0
+
+def insertDB(info, table) :
+    pass
+
+
+bigURL = "http://vip.stock.finance.sina.com.cn/q/go.php/vInvestConsult/kind/dzjy/index.phtml"
+page = 1
+while page < 2 :
+    bigTradeInfo = getPageInfo(bigURL, 60, page)
+    insertDB(bigTradeInfo, "bigtrade")
+    page = page + 1
---
+++ top.py
@@ -0,0 +1,6 @@
+#encoding: utf-8
+import urllib2
+import time
+# from bs4 import BeautifulSoup
+from BeautifulSoup import BeautifulSoup
+import MySQLdb
