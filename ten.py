e2b3737fe33cb2e72da7b38a3b8c558d56a1d517
sgzs <sgzs6721@126.com>
2016-12-06 01:04:06 +0800

    tenpercent

--- ten.py
+++ ten.py
@@ -29,45 +29,54 @@ def getSoup (url) :
 def getPageInfo(url, page) :
     realURL = url + "?page=" + str(page)
     soup = getSoup(realURL)
-    trArray = soup.find(attrs={'class':'datalist'}).table.tbody.findAll("tr")
-    print trArray
-    for tr in trArray :
+    tbody = soup.find(attrs={'class':'datalist'}).table.tbody
+    trArray = tbody.findAll("tr")
+    numArray = tbody.findAll("script")
+    # print trArray
+    for index, tr in enumerate(trArray) :
         record = {}
         td = tr.findAll("td")
         record['person']        = td[0].text.encode("utf8")
         record['prediction']    = td[3].text.encode("utf8")
+        print record['prediction']
+        record['num']           = numArray[index * 2 + 1].text.encode("utf8")[13:19]
         record['predict_time']  = td[5].text.encode("utf8")
         record['price']         = td[6].text.encode("utf8")
         record['success']       = td[9].text.encode("utf8")
+        if record['success'] == '--' :
+            record['success'] = '0'
         record['success_date']  = td[10].text.encode("utf8")
 
 
         dateAndTime = re.split('\s+', record['predict_time'])
-        print dateAndTime
         date = dateAndTime[0]
-        time = dateAndTime[1]
-        pprint(record)
-        print date
-        print time
-        exit()
+        time = dateAndTime[1].split(":")[0]
 
+        if int(time) > 14 :
+            date = str(datetime.datetime.strptime(date, "%Y-%m-%d").date() + datetime.timedelta(days=1))
 
         detail = ts.get_hist_data(record['num'], start=date)
+        print detail
+
+        if detail.empty :
+            continue
         df = detail.iloc[::-1]
 
         startCaculate = 0
-        for i in [1, 2, 5, 10, 15, 20] :
+        for i in range(0,21) :
 
             info = getPreDetail(df, i)
+            pprint(info)
 
             if info :
-                if i == 1 : startCaculate = info['open']
+                if i == 0 : startCaculate = info['open']
                 record['increase-' + str(i)] = getIncrease(startCaculate, info)
             else :
                 record['increase-' + str(i)] = ''
 
-        insertDB(record, "bigtrade")
-        # pprint('record')
+        pprint(record)
+        insertDB(record, "ten")
+        exit()
 
 def getPreDetail(df, day) :
     price = {}
@@ -75,9 +84,9 @@ def getPreDetail(df, day) :
     if len(df) > day :
         for p in ["open", "close", "high", "low"] :
             if p == 'high':
-                price[p] = max(float(x) for x in (df[p][1: day + 1]))
+                price[p] = max(float(x) for x in (df[p][0: day + 1]))
             elif p == 'low' :
-                price[p] = min(float(x) for x in (df[p][1: day + 1]))
+                price[p] = min(float(x) for x in (df[p][0: day + 1]))
             else :
                 price[p] = float(df[p][day])
 
@@ -92,36 +101,21 @@ def getIncrease(base, info) :
     return ','.join([closeIncrease, highIncrease, lowIncrease])
 
 def insertDB(info, table) :
-    print info['date'] + ":" + info['name']
+    print info['predict_time'] + ":" + info['prediction']
     cur = conn.cursor()
-    statement = "insert into " + table + "(date,num,name,dealprice,closeprice,islimited,discount," + \
-        "volume,volumemoney,dealrate,buy,sell,sameplace,increaseone,increasetwo,increasefive,increaseten," + \
-        "increasefifteen,increasetwenty,ontop,toptype) VALUES('" + info['date'] + "','" + info['num'] + "','" + \
-        info['name'] + "','" + info['dealprice'] + "','" + info['closeprice'] + "','" + \
-        info['islimited'] + "','" + info['discount'] + "','" + info['volume'] + "','" + \
-        info['volumemoney'] + "','" + info['dealrate'] + "','" + info['buy'] + "','" + \
-        info['sell'] + "','" + info['sameplace'] + "','" + info['increase-1'] + "','" + info['increase-2'] + "','" + \
+    statement = "insert into " + table + "(person,prediction,num,predict_time,success_date,price,success,increaseone,increasetwo,increasefive,increaseten," + \
+        "increasefifteen,increasetwenty) VALUES('" + info['person'] + "','" + info['prediction'] + "','" + \
+        info['num'] + "','" + info['predict_time'] + "','" + info['success_date'] + "','" + \
+        info['price'] + "','" + info['success'] + "','" + info['increase-1'] + "','" + info['increase-2'] + "','" + \
         info['increase-5'] + "','" + info['increase-10'] + "','" + info['increase-15'] + "','" + \
-        info['increase-20'] + "','" + info['ontop'] + "','" + info['toptype'] + "')"
+        info['increase-20'] + "')"
+    print statement
     try :
         cur.execute(statement)
         cur.close()
         conn.commit()
     except MySQLdb.Error, e:
         print "\tMysql Error %d: %s" % (e.args[0], e.args[1])
-        print "Update last record..."
-        volume = float(info['volume']) * 2
-        volumemoney = float(info['volumemoney']) * 2
-        dealrate = float(info['dealrate']) * 2
-        statement = "update " + table + " set volume='" + str(volume) + \
-                    "',volumemoney='" + str(volumemoney) + "',dealrate='" + \
-                    str(dealrate) + "' where dealprice='" + info['dealprice'] + "' and num='" + info['num'] + "' and volume='" + \
-                    info['volume'] + "' and buy='" + info['buy'] + "' and sell='" + info['sell'] + "'"
-        print statement
-        cur.execute(statement)
-        cur.close()
-        conn.commit()
-        pass
 
 host = "localhost"
 user = "root"
@@ -132,9 +126,9 @@ database = "stock"
 conn = MySQLdb.connect(host=host,user=user,passwd=passwd,db=database,port=port,charset='utf8')
 
 tenURL = "http://www.178448.com/fjzt-1.html"
-page = 1
+page = 43084
 
-while page > 0 :
+while page < 43085 :
     print "page:" + str(page)
     getPageInfo(tenURL, page)
     page = page + 1
