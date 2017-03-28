0ee86364be55c36df267868b8e6ceac4f4110482
sgzs <sgzs6721@126.com>
2017-03-28 23:58:49 +0800

    insert original data to db final

--- ten.py
+++ ten.py
@@ -4,9 +4,9 @@ import time
 import re
 import datetime
 from pprint import pprint
-from bs4 import BeautifulSoup
-# from BeautifulSoup import BeautifulSoup
-import tushare as ts
+# from bs4 import BeautifulSoup
+from BeautifulSoup import BeautifulSoup
+# import tushare as ts
 import MySQLdb
 
 def getSoup (url) :
@@ -15,8 +15,8 @@ def getSoup (url) :
         try :
             req = urllib2.Request(url)
             res = urllib2.urlopen(req, timeout = 15).read()
-            return BeautifulSoup(res, "html.parser")
-            # return BeautifulSoup(res,fromEncoding="gb18030")
+            # return BeautifulSoup(res, "html.parser")
+            return BeautifulSoup(res,fromEncoding="gb18030")
 
         except :
             print "Could not get soup from " + url
@@ -34,87 +34,41 @@ def getPageInfo(url, page) :
     trArray = tbody.findAll("tr")
     numArray = tbody.findAll("script")
 
-    # print trArray
     for index, tr in enumerate(trArray) :
         record = {}
         td = tr.findAll("td")
         record['person']        = td[0].text.encode("utf8")
-        record['prediction']    = td[3].text.encode("utf8")
-        record['num']           = numArray[index * 2 + 1].text.encode("utf8")[13:19]
-        record['predict_time']  = td[5].text.encode("utf8")
+        record['name']          = td[3].text.encode("utf8")
+        record['code']          = numArray[index * 2 + 1].text.encode("utf8")[13:19]
         record['price']         = td[6].text.encode("utf8")
         record['success']       = td[9].text.encode("utf8")
+
         if record['success'] == '--' :
             record['success'] = '0'
-        record['success_date']  = td[10].text.encode("utf8")
 
-
-        dateAndTime = re.split('\s+', record['predict_time'])
-        date = dateAndTime[0]
-        time = dateAndTime[1].split(":")[0]
-
-        if int(time) > 14 :
-            date = str(datetime.datetime.strptime(date, "%Y-%m-%d").date() + datetime.timedelta(days=1))
-
-        detail = ts.get_hist_data(record['num'], start=date)
-        # print detail
-
-        if detail.empty :
-            continue
-        df = detail.iloc[::-1]
-
-        startCaculate = 0
-        for i in range(0,21) :
-
-            info = getPreDetail(df, i)
-            # pprint(info)
-
-            if info :
-                if i == 0 : startCaculate = info['open']
-                record['increase-' + str(i)] = getIncrease(startCaculate, info)
-            else :
-                record['increase-' + str(i)] = ''
+        dateAndTime = re.split('\s+', td[5].text.encode("utf8"))
+        record['pdate'] = dateAndTime[0]
+        record['ptime'] = dateAndTime[1]
+        record['sdate'] = td[10].text.encode("utf8")
 
         # pprint(record)
         insertDB(record, "ten")
         # exit()
 
-def getPreDetail(df, day) :
-    price = {}
-
-    if len(df) > day :
-        for p in ["open", "close", "high", "low"] :
-            if p == 'high':
-                price[p] = max(float(x) for x in (df[p][0: day + 1]))
-            elif p == 'low' :
-                price[p] = min(float(x) for x in (df[p][0: day + 1]))
-            else :
-                price[p] = float(df[p][day])
-
-    return price
-
-def getIncrease(base, info) :
-
-    closeIncrease = str('%.2f' % ((info['close'] - base) * 100 / base))
-    highIncrease  = str('%.2f' % ((info['high']  - base) * 100 / base))
-    lowIncrease   = str('%.2f' % ((info['low']   - base) * 100 / base))
-
-    return ','.join([closeIncrease, highIncrease, lowIncrease])
-
 def insertDB(info, table) :
-    print info['person'] + ":" + info['predict_time'] + ":" + info['prediction']
+    print info['person'] + ":" + info['pdate'] + " " + info['ptime'] + ":" + info['name']
+
     cur = conn.cursor()
-    statement = "insert into " + table + "(person,prediction,num,predict_time,success_date,price,success,increaseone,increasetwo,increasefive,increaseten," + \
-        "increasefifteen,increasetwenty) VALUES('" + info['person'] + "','" + info['prediction'] + "','" + \
-        info['num'] + "','" + info['predict_time'] + "','" + info['success_date'] + "','" + \
-        info['price'] + "','" + info['success'] + "','" + info['increase-1'] + "','" + info['increase-2'] + "','" + \
-        info['increase-5'] + "','" + info['increase-10'] + "','" + info['increase-15'] + "','" + \
-        info['increase-20'] + "')"
-    # print statement
+
+    statement = "insert into " + table + "(person,name,code,price,pdate,ptime,success,sdate) VALUES('" + info['person'] + "','" + \
+        info['name'] + "','" + info['code'] + "','" + info['price'] + "','" + info['pdate'] + "','" + info['ptime'] + "','" + info['success'] + "','" + \
+        info['sdate'] + "')"
+
     try :
         cur.execute(statement)
         cur.close()
         conn.commit()
+
     except MySQLdb.Error, e:
         print "\tMysql Error %d: %s" % (e.args[0], e.args[1])
 
@@ -127,9 +81,9 @@ database = "stock"
 conn = MySQLdb.connect(host=host,user=user,passwd=passwd,db=database,port=port,charset='utf8')
 
 tenURL = "http://www.178448.com/fjzt-1.html?view=archiver"
-page = 53689
+page = 1
 
-while page > 0 :
+while page <= 53689 :
     print "page:" + str(page)
     getPageInfo(tenURL, page)
-    page = page - 1
+    page = page + 1
--- ten.sql
+++ ten.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: 2016-12-06 01:04:32
+-- Generation Time: 2017-03-28 23:57:09
 -- 服务器版本： 5.7.11
 -- PHP Version: 5.5.36
 
@@ -29,26 +29,14 @@ SET time_zone = "+00:00";
 CREATE TABLE `ten` (
   `id` int(32) NOT NULL,
   `person` varchar(32) NOT NULL,
-  `prediction` varchar(16) NOT NULL,
-  `num` varchar(8) NOT NULL,
-  `predict_time` datetime NOT NULL,
-  `success_date` varchar(64) DEFAULT NULL,
+  `name` varchar(16) NOT NULL,
+  `code` varchar(8) NOT NULL,
+  `pdate` date NOT NULL,
+  `ptime` time NOT NULL,
   `price` double DEFAULT NULL,
-  `success` int(8) DEFAULT NULL,
-  `increaseone` varchar(32) DEFAULT NULL,
-  `increasetwo` varchar(32) DEFAULT NULL,
-  `increasefive` varchar(32) DEFAULT NULL,
-  `increaseten` varchar(32) DEFAULT NULL,
-  `increasefifteen` varchar(32) DEFAULT NULL,
-  `increasetwenty` varchar(32) DEFAULT NULL
+  `sdate` varchar(16) DEFAULT NULL COMMENT AS `成功日期`,
+  `success` int(4) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- 转存表中的数据 `ten`
---
-
-INSERT INTO `ten` (`id`, `person`, `prediction`, `num`, `predict_time`, `success_date`, `price`, `success`, `increaseone`, `increasetwo`, `increasefive`, `increaseten`, `increasefifteen`, `increasetwenty`) VALUES
-(1, 'jsjs123', '银润投资', '000526', '2015-09-01 01:17:00', '', 40.28, 0, '-18.74,0.00,-20.77', '-16.12,0.00,-20.77', '-10.88,0.00,-23.18', '-19.86,0.00,-26.43', '-3.86,0.00,-26.43', '-0.72,2.42,-26.43');
 
 --
 -- Indexes for dumped tables
@@ -58,7 +46,8 @@ INSERT INTO `ten` (`id`, `person`, `prediction`, `num`, `predict_time`, `success
 -- Indexes for table `ten`
 --
 ALTER TABLE `ten`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `update` (`person`,`code`,`pdate`,`price`) USING BTREE;
 
 --
 -- 在导出的表使用AUTO_INCREMENT
@@ -68,7 +57,7 @@ ALTER TABLE `ten`
 -- 使用表AUTO_INCREMENT `ten`
 --
 ALTER TABLE `ten`
-  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
