ed8bc04d78ce8e58be2ca46b43faed1520bd4fd4
zhongshan <zhongshan@le.com>
2017-08-07 18:12:06 +0800

    complete insert to db

--- ggmm.py
+++ ggmm.py
@@ -3,11 +3,13 @@ import urllib2
 import re
 import datetime
 import time
+import math
+import sys
 
 from pprint import pprint
-# from bs4 import BeautifulSoup
-from BeautifulSoup import BeautifulSoup
-# import tushare as ts
+from bs4 import BeautifulSoup
+# from BeautifulSoup import BeautifulSoup
+import tushare as ts
 import MySQLdb
 
 def getSoup (url) :
@@ -16,8 +18,8 @@ def getSoup (url) :
         try :
             req = urllib2.Request(url)
             res = urllib2.urlopen(req, timeout = 15).read()
-            # return BeautifulSoup(res, "html.parser")
-            return BeautifulSoup(res,fromEncoding="gb18030")
+            return BeautifulSoup(res, "html.parser",from_encoding="gb18030")
+            # return BeautifulSoup(res,from_encoding="gb18030")
 
         except :
             print "Could not get soup from " + url
@@ -28,95 +30,65 @@ def getSoup (url) :
     return ""
 
 def getPageInfo(url, page) :
-    realURL = url + str(page)
-
+    realURL = url + str(page) + "/ajax/1"
     soup = getSoup(realURL)
     tbody = soup.table.tbody
     trArray = tbody.findAll("tr")
 
     trArray.reverse()
-    pprint(trArray)
-    exit()
+    # pprint(trArray)
+    # exit()
 
     for index, tr in enumerate(trArray) :
         record = {}
         td = tr.findAll("td")
-        record['person']        = td[0].text.encode("utf8")
-        record['name']          = td[3].text.encode("utf8")
-        #record['code']          = numArray[index * 2 + 1].text.encode("utf8")[13:19]
-        record['price']         = td[6].text.encode("utf8")
-        record['success']       = td[9].text.encode("utf8")
+        record['code']         = td[1].text.encode("utf8")
+        record['name']         = td[2].text.encode("utf8")
+        record['person']       = td[3].text.encode("utf8")
+        record['date']         = td[4].text.encode("utf8")
+        record['number']       = td[5].text.encode("utf8")
+        record['price']        = td[6].text.encode("utf8")
+        record['type']         = td[7].text.encode("utf8")
+        record['executives']   = td[10].text.encode("utf8")
+        record['position']     = td[12].text.encode("utf8")
+        record['relation']     = td[13].text.encode("utf8")
 
-        if record['success'] == '--' :
-            record['success'] = '0'
 
-        dateAndTime = re.split('\s+', td[5].text.encode("utf8"))
-        record['pdate'] = dateAndTime[0]
-        if record['pdate'] == "2017-03-29" :
-            exit()
-        record['ptime'] = dateAndTime[1]
-        record['sdate'] = td[10].text.encode("utf8")
+        detail = ts.get_hist_data(record['code'], start=record['date'])
+        df = detail.iloc[::-1]
+        close = df[u"close"][0]
+        record['close'] = str(close)
+
+        m = re.match(r"([0-9.-]+)(\xe4\xb8\x87)?", record['number'])
+        if record['price'] != "--" :
+            money = float(m.groups()[0]) * float(record['price'])
+            if m.groups()[1] != None :
+                money *= 10000
+            record['money'] = str(money / 10000)
+            record['discount'] =  str(round((float(record['price']) - close) / close * 100, 2))
+        else :
+            record['price'] = '0'
+            record['money'] = '0'
+            record['discount'] = '0'
 
         # pprint(record)
-        insertDB(record, "ten")
+        insertDB(record, "ggmm")
 
 def insertDB(info, table) :
-    print info['person'] + ":" + info['pdate'] + " " + info['ptime'] + ":" + info['name']
 
+    print info['name'] + "','" + info['person'] + "','" + info['date'] + "','" + info['number'] + "','" + info['price']
     cur = conn.cursor()
+    insertStatement = "insert into " + table + "(code,name,person,date,number,price,type,executives,position,relation,close,money,discount) VALUES(\"" + info['code'] + "\",'" + \
+        info['name'] + "','" + info['person'] + "','" + info['date'] + "','" + info['number'] + "','" + info['price'] + "','" + \
+        info['type'] + "','" + info['executives'] + "','" + info['position'] + "','" + info['relation'] + "','" +info['close'] + "','" +  info['money'] + "','" + info['discount'] + "')"
 
-    insertStatement = "insert into " + table + "(person,name,price,pdate,ptime,success,sdate) VALUES(\"" + info['person'] + "\",'" + \
-        info['name'] + "','" + info['price'] + "','" + info['pdate'] + "','" + info['ptime'] + "','" + info['success'] + "','" + \
-        info['sdate'] + "')"
-
-    statisticTable = "statistics"
-
-    insert = "insert into `" + statisticTable + "` (person,num,success) VALUES(\"" + info['person'] + "\",'1','"
-
-    personExist = checkExistPerson(info['person'], statisticTable)
-
-    success = "0"
-    if not info['success'] == '0':
-        success = '1'
-
-    if personExist :
-        insert = "update `" + statisticTable + "` set num = num + 1,success = success + " + success + " where id = '" + str(personExist) + "'"
-    else :
-        insert = insert + success + "')"
-
-    # return
     try :
         cur.execute(insertStatement)
-        cur.execute(insert)
-        # print "update statistics"
         cur.close()
         conn.commit()
 
     except MySQLdb.Error, e:
         print "\tMysql Error %d: %s" % (e.args[0], e.args[1])
-        conn.rollback()
-
-
-def checkExistPerson(person, table) :
-    checkStatement = "select * from `" + table + "` where person = \"" + person + "\""
-    cur = conn.cursor()
-
-    try:
-        cur.execute(checkStatement)
-        personInfo = cur.fetchall()
-        cur.close()
-        conn.commit()
-
-    except MySQLdb.Error, e:
-        print "\tCould not check whether the user is existed!"
-        exit(1)
-
-    result = 0
-    if len(personInfo) :
-        # pprint(personInfo)
-        result = personInfo[0][0]
-
-    return result
 
 
 host = "localhost"
@@ -127,7 +99,10 @@ database = "stock"
 
 conn = MySQLdb.connect(host=host,user=user,passwd=passwd,db=database,port=port,charset='utf8')
 
-ggmmURL = "http://data.10jqka.com.cn/financial/ggjy/field/enddate/order/desc/page/1/ajax/"
-page = 1
+ggmmURL = "http://data.10jqka.com.cn/financial/ggjy/field/enddate/order/desc/page/"
+page = 11
 
-getPageInfo(ggmmURL, page)
+while page > 0 :
+    print "page:" + str(page)
+    getPageInfo(ggmmURL, page)
+    page = page - 1
